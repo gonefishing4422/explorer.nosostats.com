dc9c92e7f57f233770e846e69e08d8d22c16e7ed
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-05-08 16:20:07 -0500

    Update

--- js/nosonodes.js
+++ js/nosonodes.js
@@ -17,7 +17,7 @@ fetch(url)
     let tableRows = '';
     tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-4">Node IP Address</th><th class="priority-4">Node Listening Port</th><th class="priority-2">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
     for (let line of uniqueLines) {
-      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-4">${line.nodeIp}</td><td class="priority-4">${line.nodePort}</td><td class="priority-2">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuously\u0391ctive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
+      tableRows += `<tr><td class="priority-1"><a href="getaddressbalance.html?address=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-4">${line.nodeIp}</td><td class="priority-4">${line.nodePort}</td><td class="priority-2">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuously\u0391ctive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
     }
 
     // Insert the table rows into the HTML table body
@@ -28,7 +28,7 @@ fetch(url)
 
     tableRows += `<tr><th class="priority-1">Node Address</th><th class="priority-4">Node IP Address</th><th class="priority-3">Node Listening Port</th><th class="priority-4">Country</th><th class="priority-5">Region</th><th class="priority-6">Node Uptime</th><th class="priority-7">Latitude</th><th class="priority-8">Longitude</th></tr>`;
     for (let line of uniqueLines) {
-      tableRows += `<tr><td class="priority-1"><a href="addresslookup.html?addresslookup=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-4">${line.nodePort}</td><td class="priority-4">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuously\u0391ctive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
+      tableRows += `<tr><td class="priority-1"><a href="getaddressbalance.html?address=${line.nodeAddress}" target=”_blank”>${line.nodeAddress}</a></td><td class="priority-2">${line.nodeIp}</td><td class="priority-4">${line.nodePort}</td><td class="priority-4">${line.countryAbbreviation}</td><td class="priority-5">${line.city}</td><td class="priority-6">${line.continuously\u0391ctive}</td><td class="priority-7">${line.latitude}</td><td class="priority-8">${line.longitude}</td></tr>`;
     }
     
    
